ac7b0b43aba9c530390182d5de8af9759fac1cb1
Stephen Boynton <boyntonsc@gmail.com>
2019-11-16 15:14:01 -0500

    fix url

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export async function connect() {
   try {
-    const client = await MongoClient.connect(process.env.DATABASE_URI, { useNewUrlParser: true });
+    const client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
     const db = client.db('test');
     const collection = db.collection('budget');
     return { collection, client };
